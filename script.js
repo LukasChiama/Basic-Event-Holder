2eadf5b88ab7ac094d34cc3639eef314dc8482a6
Chukwualuka Chiama <lukaschiama@gmail.com>
2018-09-30 19:03:15 +0100

    Add local storage, save, delete and update

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-function showDialog() { 
-  localStorage.setItem("name", "Oliver");
-  document.getElementsByClassName("openform")[0].style.display = "block";
-}
-
-
+let counter = -1;
 
 function storeEvent() {
+  counter += 1;
  
-  document.getElementsByClassName("openform")[0].style.display = "none";
-
   const eventHolder = new Map();
   const currentEvent = new Map();
   
-  let nameOf = document.getElementById("name").value;
-  let startDate = document.getElementById("startDate").value;
-  let endDate = document.getElementById("endDate").value;
-  let venue = document.getElementById("venue").value;
-  let price = document.getElementById("price").value;
-  let msg = document.getElementById("msg").value;
+  const event = [];
+  
+  const nameOf = document.getElementById("name").value;
+  const startDate = document.getElementById("startDate").value;
+  const endDate = document.getElementById("endDate").value;
+  const venue = document.getElementById("venue").value;
+  const price = document.getElementById("price").value;
+  const msg = document.getElementById("msg").value;
   
   currentEvent.set("Event Name", nameOf); 
   currentEvent.set("Venue", venue);
@@ -26,40 +22,75 @@ function storeEvent() {
   currentEvent.set("Ticket Price", price);
   currentEvent.set("Description", msg);
 
+  eventHolder.set("Events", currentEvent);
+
+  event.push("Event Name: " + nameOf);
+  event.push("Venue: " + startDate);
+  event.push("Starts: " + endDate);
+  event.push("Ends: " + venue);
+  event.push("Ticket Price: " + price);
+  event.push("Description: " + msg);
   
   const listArr = document.getElementById('event-list')
   const listMum = document.createElement('ul')
   const listButton1 = document.createElement('BUTTON')
   const listButton2 = document.createElement('BUTTON')
+  //listButton2.setAttribute('id', counter)
   const buttonText1 = document.createTextNode('Edit Event')
   const buttonText2 = document.createTextNode('Delete Event')
 
+    listMum.setAttribute('class', 'event-hold')
+    listMum.setAttribute('id', counter)
     listButton1.appendChild(buttonText1)
     listButton2.appendChild(buttonText2)
-    listArr.classList.add('btn-group')
+    listMum.classList.add('btn-group')
     listArr.appendChild(listMum)
     listButton1.onclick = editArr;
     listButton2.onclick = deleteArr;
-  
-  currentEvent.forEach( function (value, key) {
-    const li = document.createElement('li')
-    li.setAttribute('class', 'list-item')
-    li.innerHTML = `${key}: ${value}`
-    listMum.appendChild(li)
-  });
-    listMum.appendChild(listButton1)
-    listMum.appendChild(listButton2)
     
+
+    localStorage.setItem(counter, JSON.stringify(event));
+    const eventSafe = JSON.parse(localStorage.getItem(counter))
+ 
+    function renderEvent() {
+      eventSafe.forEach(function (key) {
+        const li = document.createElement('li')
+        li.setAttribute('class', 'list-item')
+        li.innerHTML = `${key}`
+        listMum.appendChild(li)
+      });
+        listMum.appendChild(listButton1)
+        listMum.appendChild(listButton2)
+    }
+
+    renderEvent(eventSafe);
+
   function deleteArr() {
-    if (listArr.hasChildNodes()) {
-      listArr.removeChild(listArr.childNodes[0])
+      let x = document.activeElement.parentNode.id;
+      let y = document.getElementById(x);
+      localStorage.removeItem(x);
+      listArr.removeChild(y);
+      let itemsArr = [];
+      for(let i = 0; i < (counter + 3); i++) {
+        if (localStorage.getItem(i)) {
+          item = JSON.parse(localStorage.getItem(i));
+          itemsArr.push(item);
+          
+          // renderEvent(itemsArr);
+          //console.log(i);
+          }
+        }
+        /*while(listArr.hasChildNodes()) {
+          listArr.removeChild(listArr.lastChild)
+        }*/
+      //return renderEvent();
+      console.log(itemsArr);
     }
-  }
   function editArr() {
-   if (listArr.hasChildNodes()) {
-      listArr.removeChild(listArr.lastChild)
-    }
-   return showDialog();
+      //listArr.removeChild(listArr.lastChild)
+      
+      currentEvent.set('edit', eventSafe);
+      console.log(localStorage.key(counter));
   }
 
 }
