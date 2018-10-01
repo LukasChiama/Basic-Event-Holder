73e911df296c17c2f40050ad64d0d0137c926f64
Chukwualuka Chiama <lukaschiama@gmail.com>
2018-10-01 16:54:53 +0100

    add read and edit from local storage

--- script.js
+++ script.js
@@ -1,39 +1,69 @@
-let counter = -1;
-
-function storeEvent() {
-
-  counter += 1;
- 
-  const eventHolder = new Map();
-  const currentEvent = new Map();
+function showUpcoming() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let nameOf = localStorage.key(i);
+    let items = document.createElement('li')
+    items.setAttribute('id', nameOf);
+    items = JSON.parse(localStorage.getItem(localStorage.key(i)))
+    renderEvent(items);
+    // const lis = document.getElementById('output');
+    // const li = document.createElement('ul')
+    // lis.appendChild(li);
+    // li.setAttribute('class', 'event-hold')
   
-  const event = [];
+    // items.forEach(function (key) {
+    //   const l = document.createElement('li');
+    //   l.setAttribute('class', 'list-item');
+    //   l.innerHTML = `${key}:`;
+    //   li.appendChild(l);
+    // })
+    
+}
   
+}
+
+function resetEvent() {
+  for (let i =0; i < 6; i++) {
+    document.getElementsByClassName('val')[i].value= ''; 
+  }
+}
+
+function checkName() {
+
+const nameOf = document.getElementById("name").value;
+const startDate = document.getElementById("startDate").value;
+const endDate = document.getElementById("endDate").value;
+const venue = document.getElementById("venue").value;
+const price = document.getElementById("price").value;
+const msg = document.getElementById("msg").value;
+
+  if (nameOf === '') {
+    return alert('Event Name is required. Please input!');
+  } else {
+    return storeEvent();
+  }
+
+  function storeEvent() {
+
+    const eventHolder = new Map();
+  
+    eventHolder.set("Event Name", nameOf); 
+    eventHolder.set("Venue", venue);
+    eventHolder.set("Starts", startDate);
+    eventHolder.set("Ends", endDate);
+    eventHolder.set("Ticket Price", price);
+    eventHolder.set("Description", msg);
+  
+    localStorage.setItem(nameOf, JSON.stringify(Array.from(eventHolder.entries())));
+    const eventSafe = new Map(JSON.parse(localStorage.getItem(nameOf)));
+    return  renderEvent(eventSafe);
+  }
+}
+
+function renderEvent(arr) {
+
   const nameOf = document.getElementById("name").value;
-  const startDate = document.getElementById("startDate").value;
-  const endDate = document.getElementById("endDate").value;
-  const venue = document.getElementById("venue").value;
-  const price = document.getElementById("price").value;
-  const msg = document.getElementById("msg").value;
-
   
-  currentEvent.set("Event Name", nameOf); 
-  currentEvent.set("Venue", venue);
-  currentEvent.set("Starts", startDate);
-  currentEvent.set("Ends", endDate);
-  currentEvent.set("Ticket Price", price);
-  currentEvent.set("Description", msg);
-
-  eventHolder.set("Events", currentEvent);
-
-  event.push("Event Name: " + nameOf);
-  event.push("Venue: " + startDate);
-  event.push("Starts: " + endDate);
-  event.push("Ends: " + venue);
-  event.push("Ticket Price: " + price);
-  event.push("Description: " + msg);
-  
-  const listArr = document.getElementById('event-list')
+  const listArr = document.getElementById('output')
   const listMum = document.createElement('ul')
   const listButton1 = document.createElement('BUTTON')
   const listButton2 = document.createElement('BUTTON')
@@ -48,27 +78,18 @@ function storeEvent() {
     listArr.appendChild(listMum)
     listButton1.onclick = editArr;
     listButton2.onclick = deleteArr;
-    
 
-    localStorage.setItem(nameOf, JSON.stringify(Array.from(currentEvent.entries())));
-    const eventSafe = new Map(JSON.parse(localStorage.getItem(nameOf)));
-
-    // localStorage.setItem(counter, JSON.stringify(event));
-    // const eventSafe = JSON.parse(localStorage.getItem(counter));
-
-    function renderEvent() {
-      eventSafe.forEach(function (value, key) {
-        //for (let [key, value] of eventSafe) {
+        
+    for (let [key, value] of arr) {
         const li = document.createElement('li')
         li.setAttribute('class', 'list-item')
         li.innerHTML = `${key}: ${value}`
         listMum.appendChild(li)
-      });
-        listMum.appendChild(listButton1)
-        listMum.appendChild(listButton2)
-    }
-
-    renderEvent(eventSafe);
+      };
+      listMum.appendChild(listButton1)
+      listMum.appendChild(listButton2)
+    
+  
 
     for (let i =0; i < 6; i++) {
       document.getElementsByClassName('val')[i].value= ''; 
@@ -79,26 +100,18 @@ function storeEvent() {
       let y = document.getElementById(x);
       localStorage.removeItem(x);
       listArr.removeChild(y);
-      // for (let i =0; i < 6; i++) {
-      //   document.getElementsByClassName('val')[i].value= ''; 
-      // }
     }
   function editArr() {
       let a = document.activeElement.parentNode.id;
       let b = document.getElementById(a);
       let c = JSON.parse(localStorage.getItem(a));
-      console.log(c);
-      //listArr.removeChild(b);
-
-      // for (let i = 0; i < 6; i++) {
-      //   document.getElementsByClassName('val')[i].value = c[i];
-      // }
       listArr.removeChild(b);
-      for (let i = 0; i < 6; i++) {
-      // i = 0;
-      // c.forEach(function () {
-        document.getElementsByClassName('val')[i].value = c[i][1];
-      };
-  }
 
+      i = 0;
+      c.forEach(function () {
+        document.getElementsByClassName('val')[i].value = c[i][1];
+        i++
+      });
+  }
 }
+ // renderEvent(arr);
